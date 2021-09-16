8b1fac7c9e3970ed3b21f08ac1ed9a42b6d6625b
Gbolagade winner <gbolagadewinner@gmail.com>
2021-09-15 21:21:44 -0400

    Night and day background successfully added

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,84 @@
 <template>
-<div>
-  <div class="container-fluid bg-dark text-white">
-  <div class="card-header d-flex main">
-    <h5>Devayo Weather App</h5>
-    <p class="ms-auto">Welcome To Devayo Weather App</p>
-  </div>
-</div>
-<div class="mains">
-  <div class="container">
-  <div class="row">
-    <div class="col-md-4 mt-4 ms-auto">
-      <div class="card-header">
-        Search Section
+  <div>
+    <div class="container-fluid bg-dark text-white">
+      <div class="card-header d-flex main">
+        <h5>Devayo Weather App</h5>
+        <p class="ms-auto">Welcome To Devayo Weather App</p>
       </div>
-      <div class="card-body border">
-      <form v-on:submit.prevent="getWeather">
-         <div class="form-group">
-            <input type="text" placeholder="What City?" class="form-control" v-model="citySearch" autocomplete="off"> 
-            <button class="btn btn-success mt-3" type="submit">Submit</button>
+    </div>
+    <div class="mains" :class="isDay ? 'light-theme' : 'dark-theme'">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 mt-4 ms-auto">
+            <div class="card-header">Search Section</div>
+            <div class="card-body border">
+              <form v-on:submit.prevent="getWeather">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    placeholder="What City?"
+                    class="form-control"
+                    v-model="citySearch"
+                    autocomplete="off"
+                  />
+                  <button class="btn btn-success mt-3" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div class="col-md-8 mt-4">
+            <div class="card-header">
+              <div class="d-flex">
+                <p>Display Section</p>
+                <h5 v-if="isDay" class="ms-auto">Day-Time</h5>
+                <h5 v-else class="ms-auto">Night</h5>
+              </div>
+            </div>
+            <div class="card-body border">
+              <h4 class="text-center">{{ weather.cityName }}</h4>
+              <span
+                ><h5 class="text-center">{{ weather.country }}</h5></span
+              >
+              <div class="text-center">
+                <span
+                  ><h2 class="mt-5">{{ weather.temperature }}°C</h2></span
+                >
+                <small>{{ weather.description }}</small>
+              </div>
+              <div class="d-flex container justify-content-end">
+                <p class="ml-auto">{{ weather.lowTemp }}°C</p>
+                <p class="ms-auto">{{ weather.highTemp }}°C</p>
+              </div>
+
+              <div class="d-flex container justify-content-end mt-4">
+                <div class="ml-auto">
+                  <h3>{{ weather.feelsLike }}°C</h3>
+                  <small>Feels Like</small>
+                </div>
+                <div class="ms-auto">
+                  <h3>{{ weather.humidity }}°C</h3>
+                  <small>Humidity</small>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer">
+              <p>Application by Devayo</p>
+            </div>
+          </div>
         </div>
-      </form>
-      </div>
-    </div>
-
-     <div class="col-md-8 mt-4">
-      <div class="card-header">
-        Display Section
-      </div>
-      <div class="card-body border">
-        <h4 class="text-center">{{weather.cityName}}</h4>
-        <span><h5 class="text-center">{{weather.country}}</h5></span>
-      <div class="text-center">
-         <span><h2 class="mt-5">{{weather.temperature}}°C</h2></span>
-         <small>{{weather.description}}</small>
-      </div>
-      <div class="d-flex container justify-content-end">
-        <p class="ml-auto">{{weather.lowTemp}}°C</p>
-        <p class="ms-auto">{{weather.highTemp}}°C</p>
-      </div>
-
-      <div class="d-flex container justify-content-end mt-4">
-       <div class="ml-auto">
-          <h3>{{weather.feelsLike}}°C</h3>
-          <small>Feels Like</small>
-       </div>
-       <div class="ms-auto">
-          <h3>{{weather.humidity}}°C</h3>
-          <small>Humidity</small>
-       </div>
-      </div>
-      </div>
-      <div class="card-footer">
-        <p>Application by Devayo</p>
       </div>
     </div>
   </div>
-</div>
-</div>
-</div>
-
 </template>
 
 <script>
-
-
 export default {
-  data(){
-    return{
+  data() {
+    return {
+      isDay: true,
       citySearch: "",
       weather: {
         cityName: "Abuja",
@@ -76,31 +88,58 @@ export default {
         lowTemp: "19",
         highTemp: "24",
         feelsLike: "20",
-        humidity: "55"
-
-      }
+        humidity: "55",
+      },
     };
   },
   methods: {
-    getWeather: async function(){
+    getWeather: async function () {
       console.log(this.citySearch);
       const key = "87aee6a845d121670d18ea1a1c036571";
       const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${this.citySearch}&appid=${key}&units=metric`;
+
+      const response = await fetch(baseURL);
+      const data = await response.json();
+      console.log(data);
       this.citySearch = "";
+      this.weather.cityName = data.name;
+      this.weather.country = data.sys.country;
+      this.weather.temperature = Math.round(data.main.temp);
+      this.weather.description = data.weather[0].description;
+      this.weather.lowTemp = Math.round(data.main.temp_min);
+      this.weather.highTemp = Math.round(data.main.temp_max);
+      this.weather.feelsLike = Math.round(data.main.feels_like);
+      this.weather.humidity = Math.round(data.main.humidity);
+      const DayNight = data.weather[0].icon;
+
+      //Check the time of the day
+      if (DayNight.includes("n")) {
+        this.isDay = false;
+      } else {
+        this.isDay = true;
+      }
     },
   },
-}
+};
 </script>
 
 <style>
-  .mains{
-    background-color: aliceblue;
-    height: calc(100vh - 58px);
-    width: 100vw;
+.mains {
+  height: calc(100vh - 58px);
+  width: 100vw;
+}
+
+.light-theme {
+  background: linear-gradient(to bottom left, #d7d3ac, #ffffff);
+}
+
+.dark-theme {
+  background: linear-gradient(to bottom left, #4854a2, #3d3d3d);
+  color: white;
+}
+@media screen and (max-width: 539px) {
+  .main p {
+    display: none;
   }
-  @media screen and (max-width: 539px) {
-    .main p{
-      display: none;
-    }
 }
 </style>
